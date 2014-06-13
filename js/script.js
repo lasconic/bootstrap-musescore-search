b370b120ec072b1845530de2957ec6fdeff316b2
Thomas Bonte <thomas.bonte@gmail.com>
2014-06-13 13:06:16 +0200

    update

--- js/script.js
+++ js/script.js
@@ -23,23 +23,23 @@ $(document).ready(function() {
             var nid = $(this).attr("data-nid");
             var secret = $(this).attr("data-secret");
             $.getJSON('http://musescore.com/oembed/endpoint?url='+url+'&format=jsonp&callback=?', function(data) {
-                $('#main').html('<div>Render this score to <a href="#" class="score_to_motion">Motion</a>, <a href="#" class="score-to-browser">Browser</a>, <a href="#" class="score-to-laser">Laser</a></div><div id="main-render">'+data.html+'</div>');
-            });
-            $('.score_to_motion').click(function(){
-              $.get("http://localhost:5000/convert?url=" + url, function(data) { 
-                console.log(data);
-                $('#main-render').html('<video width="100%" height="100%" controls="" autoplay="" name="media"><source src="' + data + '" type="video/mp4"></video>');
-              });
-              return false;
-            });
-            $('.score_to_browser').click(function(){
-              var h = $(document).height() - 50;
-              $('#main-render').html('<iframe width="100%" height="' + h + '" frameborder="0" src="http://localhost:8000/score.html?nid='+ nid +'&secret='+ secret+'">')
-              return false;
-            });
-            $('.score_to_laser').click(function(){
-              $('#main-render').html('Here comes the video of the laser printer');
-              return false;
+                $('#main').html('<div>Render this score to <a href="#" class="score-to-motion">Motion</a>, <a href="#" class="score-to-browser">Browser</a>, <a href="#" class="score-to-laser">Laser</a></div><div id="main-render">'+data.html+'</div>');
+                $('.score-to-motion').click(function(){
+                  $.get("http://localhost:5000/convert?url=" + url, function(data) { 
+                    console.log(data);
+                    $('#main-render').html('<video width="100%" height="100%" controls="" autoplay="" name="media"><source src="' + data + '" type="video/mp4"></video>');
+                  });
+                  return false;
+                });
+                $('.score-to-browser').click(function(){
+                  var h = $(document).height() - 50;
+                  $('#main-render').html('<iframe width="100%" height="' + h + '" frameborder="0" src="http://localhost:8000/score.html?nid='+ nid +'&secret='+ secret+'">')
+                  return false;
+                });
+                $('.score-to-laser').click(function(){
+                  $('#main-render').html('Here comes the video of the laser printer');
+                  return false;
+                });
             });
             return false;
         });        
