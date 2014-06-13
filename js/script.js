b850a621460a38d331e0db81752f61797c2336f0
Thomas Bonte <thomas.bonte@gmail.com>
2014-06-13 14:25:04 +0200

    update

--- js/script.js
+++ js/script.js
@@ -23,8 +23,9 @@ $(document).ready(function() {
             var nid = $(this).attr("data-nid");
             var secret = $(this).attr("data-secret");
             $.getJSON('http://musescore.com/oembed/endpoint?url='+url+'&format=jsonp&callback=?', function(data) {
-                $('#main').html('<div>Render this score to <a href="#" class="score-to-motion">Motion</a>, <a href="#" class="score-to-browser">Browser</a>, <a href="#" class="score-to-laser">Laser</a></div><div id="main-render">'+data.html+'</div>');
+                $('#main').html('<div class="main-text">Render this score to <a href="#" class="score-to-motion">Motion</a>, <a href="#" class="score-to-browser">Browser</a>, <a href="#" class="score-to-laser">Laser</a></div><div id="main-render">'+data.html+'</div>');
                 $('.score-to-motion').click(function(){
+                  $('#main-render').html('<center>Conversion to MP4 ongoing. Please hold! <br><br><img src="http://www.sensiblepm.com/wp-content/uploads/2013/06/loader_spinner.gif" /></center>');
                   $.get("http://localhost:5000/convert?url=" + url, function(data) { 
                     console.log(data);
                     $('#main-render').html('<video width="100%" height="100%" controls="" autoplay="" name="media"><source src="' + data + '" type="video/mp4"></video>');
@@ -37,7 +38,7 @@ $(document).ready(function() {
                   return false;
                 });
                 $('.score-to-laser').click(function(){
-                  $('#main-render').html('Here comes the video of the laser printer');
+                  $('#main-render').html('Get your score laser edged in one of the <a href="http://fablab.io">Fab Labs around the world</a>.<br><br> <iframe width="560" height="315" src="//www.youtube.com/embed/wJCMWKB3goI?autoplay=1" frameborder="0" allowfullscreen></iframe> <br> Sample by <a href="http://made-bcn.org">Made BCN</a> for <a href="http://new.musichackday.org/2014/barcelona/">MusicHackDay Barcelona 2014</a>.');
                   return false;
                 });
             });
