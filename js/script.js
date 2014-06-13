48db653c6afafa176b9335499626631bacb24dc7
Thomas Bonte <thomas.bonte@gmail.com>
2014-06-13 13:01:25 +0200

    update

--- js/script.js
+++ js/script.js
@@ -10,21 +10,40 @@ $(document).ready(function() {
             $.getJSON('http://api.musescore.com/services/rest/score.jsonp?&oauth_consumer_key=musichackday&callback=?&text=' + text, function(data) {
                 $('#results').html('');
                 $.each(data, function(index, score) {
-                    $('#results').append('<li data-url="' + score.permalink + '" class="score-result"><img src="http://static.musescore.com/' + score.id + '/' + score.secret + '/thumb.png?nocache=' + score.dates.lastupdate + '"><a href="#">' + score.title + '</a></li>');
+                    $('#results').append('<li data-nid="'+score.id+'" data-secret="'+score.secret+'" data-url="' + score.permalink + '" class="score-result"><a href="#"><img src="http://static.musescore.com/' + score.id + '/' + score.secret + '/thumb.png?nocache=' + score.dates.lastupdate + '" class="score-picture"><div class="score-title">' + score.title + '</div></a></li>');
                 });;
-                $('.score-result').click(function(event) {
-                    var url = $(this).attr("data-url");
-                    $.get("http://localhost:5000/convert?url=" + url, function(data) { 
-                        console.log(data);
-                        $('#main').html(' <video width="100%" height="100%" controls="" autoplay="" name="media"><source src="' + data + '" type="video/mp4"></video>')
-
-                        //$('#main').html('<a href="' + data + '">Watch the video</a>');
-                    })
-                });
+                score_oembed();
             });
         }
     });
 
-
+    function score_oembed() {
+        $('.score-result').click(function(event) {
+            var url = $(this).attr("data-url");
+            var nid = $(this).attr("data-nid");
+            var secret = $(this).attr("data-secret");
+            $.getJSON('http://musescore.com/oembed/endpoint?url='+url+'&format=jsonp&callback=?', function(data) {
+                $('#main').html('<div>Render this score to <a href="#" class="score_to_motion">Motion</a>, <a href="#" class="score-to-browser">Browser</a>, <a href="#" class="score-to-laser">Laser</a></div><div id="main-render">'+data.html+'</div>');
+            });
+            $('.score_to_motion').click(function(){
+              $.get("http://localhost:5000/convert?url=" + url, function(data) { 
+                console.log(data);
+                $('#main-render').html('<video width="100%" height="100%" controls="" autoplay="" name="media"><source src="' + data + '" type="video/mp4"></video>');
+              });
+              return false;
+            });
+            $('.score_to_browser').click(function(){
+              var h = $(document).height() - 50;
+              $('#main-render').html('<iframe width="100%" height="' + h + '" frameborder="0" src="http://localhost:8000/score.html?nid='+ nid +'&secret='+ secret+'">')
+              return false;
+            });
+            $('.score_to_laser').click(function(){
+              $('#main-render').html('Here comes the video of the laser printer');
+              return false;
+            });
+            return false;
+        });        
+    }
+    score_oembed();
 
 });
